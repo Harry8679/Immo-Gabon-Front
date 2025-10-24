0572ae4c864a6f778ff693b41a6ae09173c02338
Harry7986 <emarh.harry.code@gmail.com>
2025-10-24 09:51:36 +0200

    Annonces 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Navbar from './components/layout/Navbar';
+import Annonces from './pages/Annonces';
 
 const App = () => {
 
@@ -15,6 +16,7 @@ const App = () => {
           <Route path='/' element={<Home />}/>
           <Route path='/inscription' element={<Register />}/>
           <Route path='/connexion' element={<Login />}/>
+          <Route path='/annonces' element={<Annonces />}/>
         </Routes>
       </div>
     </Router>
--- src/pages/Annonces.tsx
+++ src/pages/Annonces.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 const Annonces = () => {
   return (
     <div>Annonces</div>
   )
 }
 
-export default Annonces;
+export default Annonces
