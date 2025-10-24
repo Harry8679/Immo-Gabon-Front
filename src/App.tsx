0391fa24f5f427220b14778827841c59536808e0
Harry7986 <emarh.harry.code@gmail.com>
2025-10-24 07:25:49 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import './App.css'
+// import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Navbar from './components/layout/Navbar';
 
-function App() {
+const App = () => {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <Router>
+      <div className="min-h-screen bg-white">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/inscription' element={<Register />}/>
+          <Route path='/connexion' element={<Login />}/>
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
