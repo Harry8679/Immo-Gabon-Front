9712b7a88f1255d8cc654833b07e1ace04fc6bdb
Harry7986 <emarh.harry.code@gmail.com>
2025-11-03 20:05:55 +0100

    Auth Slice 3

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -15,3 +15,19 @@ interface AuthState {
 }
 
 // Thunk pour l'inscription
+export const RegisterUser = createAsyncThunk("auth/registerUser", async(formData: { 
+    firstName: string,
+    lastName: string;
+    email: string;
+    password: string;
+ }, thunkAPI) => {
+    try {
+        const res = await axios.post("http://localhost:8001/api/register", formData, {
+            headers: {"Content-Type": "application/json"},
+        });
+        // On suppose que l'API renvoie un objet { message: user }
+        return res.data;
+    } catch (error: any) {
+        return thunkAPI.rejectWithValue(error.response?.data?.message || "Erreur d'inscription");
+    }
+ });
